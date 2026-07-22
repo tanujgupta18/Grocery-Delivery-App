5f86be9b0779b92146736c486ca3e97b23709fbe
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-07-22 17:57:08 +0530

    Categories section

--- client/src/assets/assets.ts
+++ client/src/assets/assets.ts
@@ -1,4 +1,14 @@
 import { TruckIcon, LeafIcon, ClockIcon, ShieldCheckIcon } from "lucide-react";
+import fruits_vegetables from "./fruits_vegetables.png";
+import dairy_eggs from "./dairy_eggs.png";
+import bakery from "./bakery.png";
+import drinks from "./drinks.png";
+import pantry_staples from "./pantry_staples.png";
+import snacks from "./snacks.png";
+import frozen_foods from "./frozen_foods.png";
+import personal_care from "./personal_care.png";
+import baby_care from "./baby_care.png";
+import meat_seafood from "./meat_seafood.png";
 
 import hero_bg from "./hero_bg.jpeg";
 
@@ -17,3 +27,20 @@ export const heroSectionData = {
     { icon: ShieldCheckIcon, title: "Secure Pay", desc: "Safe checkout" },
   ],
 };
+
+export const categoriesData = [
+  {
+    slug: "fruits-vegetables",
+    name: "Fruits & Vegetables",
+    image: fruits_vegetables,
+  },
+  { slug: "personal-care", name: "Personal Care", image: personal_care },
+  { slug: "pantry-staples", name: "Pantry Staples", image: pantry_staples },
+  { slug: "bakery", name: "Bakery", image: bakery },
+  { slug: "beverages", name: "Beverages", image: drinks },
+  { slug: "meat-seafood", name: "Meat & Seafood", image: meat_seafood },
+  { slug: "snacks", name: "Snacks", image: snacks },
+  { slug: "frozen-foods", name: "Frozen Foods", image: frozen_foods },
+  { slug: "baby-care", name: "Baby Care", image: baby_care },
+  { slug: "dairy-eggs", name: "Dairy & Eggs", image: dairy_eggs },
+];
